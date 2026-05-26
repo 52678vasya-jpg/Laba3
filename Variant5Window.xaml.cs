1b44a30eb80bcfb25bea23cdd0a3a95b985ce633
Damir11111 <damirmtmustafin@gmail.com>
2026-05-26 03:16:24 +0300

    инициализация формы

---
+++ Variant5Window.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
+
