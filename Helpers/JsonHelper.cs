7a78ceff5dd6b1694ee920d1ffe5657eb3add586
player404 <52678vasya@gmail.com>
2026-05-26 03:44:59 +0300

    Работа с данными
    загрузка и обработка данных

---
+++ Helpers/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using InflationAnalyzer.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace InflationAnalyzer.Helpers
+{
+    public static class JsonHelper
+    {
+        // Загрузка JSON файла
+        public static List<InflationData> LoadJson(
+            string path)
+        {
+            string json =
+                File.ReadAllText(path);
+
+            return JsonSerializer.Deserialize
+                <List<InflationData>>(json);
+        }
+    }
+}
