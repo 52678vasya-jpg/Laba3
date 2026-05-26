defd51cd7d7080a727bf4808174bb7987d897b52
Damir11111 <damirmtmustafin@gmail.com>
2026-05-26 03:23:44 +0300

    вывод данных в таблицу

--- Variant5Window.xaml.cs
+++ Variant5Window.xaml.cs
@@ -8,3 +8,36 @@ using OxyPlot;
 using OxyPlot.Series;
 using OxyPlot.Wpf;
 
+namespace PopulationApp
+{
+    public partial class Variant5Window : Window
+    {
+        private List<PopulationRecord> _historicalData;
+        private readonly IAnalyzer _analyzer;
+        private PlotModel _plotModel;
+
+        public Variant5Window()
+        {
+            InitializeComponent();
+            _analyzer = new PopulationAnalyzer();
+            InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+            _plotModel = new PlotModel { Title = "Численность населения России" };
+            _plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, Title = "Год" });
+            _plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, Title = "Население (млн)" });
+            PlotPopulation.Model = _plotModel;
+        }
+
+        private void LoadData_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string json = File.ReadAllText("population.json");
+                _historicalData = JsonSerializer.Deserialize<List<PopulationRecord>>(json);
+
+                DataGridPopulation.ItemsSource = _historicalData; // Вывод в таблицу 
+
+                
